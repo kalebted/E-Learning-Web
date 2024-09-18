98c0fcceb90a7235e93d6c88bc48fae4f5cc100c
Kaleb Tedla <kalebttesfaye@gmail.com>
2024-09-18 19:56:59 +0300

    Add framer motion to parts

--- e-learning/src/components/Hero/Hero.jsx
+++ e-learning/src/components/Hero/Hero.jsx
@@ -36,24 +36,34 @@ const Hero = () => {
                 <div className="flex flex-col justify-center py-14 md:py-0 relative z-20">
                     <div className="text-center md:text-left
                     space-y-10 lg:max-w-[400px]">
-                        <h1 className="text-3xl lg:text-5xl font-bold
+                        <motion.h1
+                        variants={FadeUp(0.6)}
+                        initial="initial"
+                        animate="animate"
+                        className="text-3xl lg:text-5xl font-bold
                         !leading-snug">
                             Let&apos;s Learn to build a{" "}
                             <span className="text-secondary">Website</span> for your business
-                        </h1>
-                        <div className="flex justify-center md:justify-start">
+                        </motion.h1>
+                        <motion.div
+                        variants={FadeUp(0.8)}
+                        initial="initial"
+                        animate="animate"
+                        className="flex justify-center md:justify-start">
                             <button className="primary-btn flex items-center gap-2 group">
                                 Get Started
                                 <IoIosArrowRoundForward className="text-xl
                             group-hover:translate-x-2
                             group-hover:-rotate-45 duration-300" />
                             </button>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
                 {/* Hero Image */}
                 <div className="flex justify-center items-center">
-                    <img src={HeroPng}
+                    <motion.img
+                         initial={{x:50}}
+                        src={HeroPng}
                         alt=""
                         className="w-[400px] xl:w-[600px] relative z-10
                         drop-shadow"
