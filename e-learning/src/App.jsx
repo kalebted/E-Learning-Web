fb37393e5649dd3b2294cceb6ea0d9c37e5ab7cf
Kaleb Tedla <kalebttesfaye@gmail.com>
2024-09-19 11:02:25 +0300

    Start the services section

--- e-learning/src/App.jsx
+++ e-learning/src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Hero from './components/Hero/Hero';
+import Services from './components/Services/Services';
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
-      <Hero />      
+      <Hero />
+      <Services />     
     </main>
   );
 }
