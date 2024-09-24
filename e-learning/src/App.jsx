fbf45ab48888dd6c0e28088e75ee75a59f2f62ba
Kaleb Tedla <kalebttesfaye@gmail.com>
2024-09-24 14:21:48 +0300

    Completed the subscribe section

--- e-learning/src/App.jsx
+++ e-learning/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
 import Banner from './components/Banner/Banner';
+import Subscribe from './components/Subscribe/Subscribe';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <Services /> 
       <Banner />    
+      <Subscribe />
     </main>
   );
 }
