42c31caf747b8caaaa89d48a6f48579288c51e79
Kaleb Tedla <kalebttesfaye@gmail.com>
2024-09-24 11:26:51 +0300

    Completed the banner section

--- e-learning/src/App.jsx
+++ e-learning/src/App.jsx
@@ -1,13 +1,14 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
+import Banner from './components/Banner/Banner';
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <Hero />
-      <Services />     
+      <Services /> 
+      <Banner />    
     </main>
   );
 }
