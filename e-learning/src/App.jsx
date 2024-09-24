3dc353fd3992abde83369c02848621851cccacc2
Kaleb Tedla <kalebttesfaye@gmail.com>
2024-09-24 14:38:31 +0300

    Completed lower banner

--- e-learning/src/App.jsx
+++ e-learning/src/App.jsx
@@ -3,14 +3,16 @@ import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
 import Banner from './components/Banner/Banner';
 import Subscribe from './components/Subscribe/Subscribe';
+import Banner2 from './components/Banner/Banner2';
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <Hero />
       <Services /> 
-      <Banner />    
+      <Banner />  
       <Subscribe />
+      <Banner2 />
     </main>
   );
 }
